96f327b5cd7074adff43e622fc1831c6ed6b4b9c
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-10-01 15:21:37 -0700

    added middleware for bookRoute to refactor the code

--- routers/bookRoute.js
+++ routers/bookRoute.js
@@ -33,21 +33,47 @@ var routes = function(Books) {
 
         });
 
+    //middleware implementation specifically for /:bookId 
+    bookRouter.use('/:bookId', function(req, res, next) {
+
+        Books.findById(req.params.bookId, function(err, book) {
+            if (err) {
+                console.error(err);
+            } else if (book) {
+                req.book = book;
+                next();
+            } else {
+                res.status(404).send('no book found');
+            }
+        });
+    });
     bookRouter.route('/:bookId')
         .get(function(req, res) {
+            //obsolete after implmenting middleware
+            /*
             Books.findById(req.params.bookId, function(err, book) {
                 res.json(book);
-            });
+            });*/
+            res.json(req.book);
         })
-        .put(function(req,res){
-            Book.findById(req.params.bookId, function(err, book){
+        .put(function(req, res) {
+
+            //obsolete after implmenting middleware
+            /*
+            Books.findById(req.params.bookId, function(err, book) {
                 book.title = req.body.title;
                 book.author = req.body.author;
                 book.genre = req.body.genre;
                 book.read = req.body.read;
                 book.save();
                 res.json(book);
-            });
+            });*/
+            req.book.title = req.body.title;
+            req.book.author = req.body.author;
+            req.book.genre = req.body.genre;
+            req.book.read = req.body.read;
+            req.book.save();
+            res.json(book);
         });
 
     return bookRouter;
