2135ee81c60a9b1747aaccbe1bb7fa904870e337
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-10-01 15:29:44 -0700

    added patch and delete verbs for /api/books/:bookId

--- routers/bookRoute.js
+++ routers/bookRoute.js
@@ -72,8 +72,40 @@ var routes = function(Books) {
             req.book.author = req.body.author;
             req.book.genre = req.body.genre;
             req.book.read = req.body.read;
-            req.book.save();
-            res.json(book);
+            req.book.save(function(err) {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+                    res.json(req.book);
+                }
+            });
+        })
+        .patch(function(req, res) {
+            if (req.body._id) {
+                delete req.body._id;
+            }
+
+            for (var p in req.body) {
+                req.book[p] = req.body[p];
+            }
+            req.book.save(function(err) {
+                if (err) {
+                    res.status(500).send(err);
+                } else {
+
+                    res.json(res.book);
+                }
+            });
+
+        })
+        .delete(function(req, res){
+            req.book.remove(function(err){
+                if(err){
+                    res.status(500).send(err);
+                } else {
+                    res.status(204).send("removed");
+                }
+            });
         });
 
     return bookRouter;
